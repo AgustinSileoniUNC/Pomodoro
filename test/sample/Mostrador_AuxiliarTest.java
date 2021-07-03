9c439036407ab3904ffc919415798a7008e0970e
Agustin Sileoni <agustinsileoni@mi.unc.edu.ar>
2021-07-03 14:21:16 -0300

    Añadido de Circleci

---
+++ test/sample/Mostrador_AuxiliarTest.java
@@ -0,0 +1,12 @@
+package sample;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Mostrador_AuxiliarTest {
+
+    @Test
+    void actualizar() {
+    }
+}
